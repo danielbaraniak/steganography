a69fae224cee61ccc4424487aa0689517dbd0a8d
230443 <230443@edu.p.lodz.pl>
2022-12-06 02:27:38 +0100

    add class based Iwt

--- stego/transform/dwt.py
+++ stego/transform/dwt.py
@@ -7,7 +7,7 @@ import pywt
 
 class Transform(ABC):
     @abstractmethod
-    def forward(self):
+    def forward(self, arr):
         pass
 
     @abstractmethod
@@ -33,22 +33,30 @@ class Dwt(Transform):
         return pywt.idwt2(coeffs=self.coeffs, wavelet=self.wavelet, mode=self.mode, axes=self.axes)
 
 
-class Iwt(Dwt):
-    def __init__(self, wavelet, mode='symmetric', axes=(-2, -1), scale=2) -> Self:
-        super().__init__(wavelet, mode, axes)
-        self.scale = scale
+class Iwt(Transform):
+    def __init__(self, wavelet, level=1, scale=None):
+        self.wavelet = wavelet
+        self.level = level
+        self.coefficients = None
+        if scale:
+            self.scale = scale
+        else:
+            self.scale = 2**level
 
     def forward(self, img):
-        coeffs = super().forward(img)
-        ll, (lh, hl, hh) = coeffs
-        arr = np.array([ll, lh, hl, hh])
-        arr = np.rint(arr * self.scale).astype(int)
-        self.coeffs = [arr[0], [arr[1], arr[2], arr[3]]]
-        return self.coeffs
+        self.coefficients = pywt.wavedec2(img, wavelet=self.wavelet, level=self.level)
+        self._to_int()
 
     def inverse(self):
-        ll, (lh, hl, hh) = self.coeffs
-        arr = np.array([ll, lh, hl, hh])
+        self._inverse_to_int()
+        return pywt.waverec2(self.coefficients, wavelet=self.wavelet)
+
+    def _to_int(self):
+        arr, coeff_slices = pywt.coeffs_to_array(self.coefficients)
+        arr = np.rint(arr * self.scale).astype(int)
+        self.coefficients = pywt.array_to_coeffs(arr, coeff_slices, output_format='wavedec2')
+
+    def _inverse_to_int(self):
+        arr, coeff_slices = pywt.coeffs_to_array(self.coefficients)
         arr = (arr / self.scale)
-        self.coeffs = [arr[0], [arr[1], arr[2], arr[3]]]
-        return super().inverse()
+        self.coefficients = pywt.array_to_coeffs(arr, coeff_slices, output_format='wavedec2')
