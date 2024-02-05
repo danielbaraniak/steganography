78a3ccc1a054fe3ac9dcef667c9df831e75dcba8
Daniel Baraniak <dbaraniak2000@gmail.com>
2024-02-05 12:27:01 +0100

    Fix color spaces conversion

--- stego/core/multichannel_coder.py
+++ stego/core/multichannel_coder.py
@@ -17,20 +17,46 @@ color_spaces = {
     "XYZ": cv2.COLOR_RGB2XYZ,
 }
 
+color_spaces_inversed = {
+    "RGB": None,
+    "YCrCb": cv2.COLOR_YCrCb2RGB,
+    "HSV": cv2.COLOR_HSV2RGB,
+    "HLS": cv2.COLOR_HLS2RGB,
+    "Lab": cv2.COLOR_Lab2RGB,
+    "Luv": cv2.COLOR_Luv2RGB,
+    "YUV": cv2.COLOR_YUV2RGB,
+    "XYZ": cv2.COLOR_XYZ2RGB,
+}
+
+def rgb_to_color_space(image: np.ndarray, color_space: str) -> np.ndarray:
+    """Converts an image to a different color space."""
+    if color_space == "RGB":
+        return image
+    else:
+        return cv2.cvtColor(image, color_spaces.get(color_space, cv2.COLOR_RGB2YCrCb))
+
+
+def color_space_to_rgb(image: np.ndarray, color_space: str) -> np.ndarray:
+    """Converts an image from a different color space to RGB."""
+    if color_space == "RGB":
+        return image
+    else:
+        return cv2.cvtColor(image, color_spaces_inversed.get(color_space, cv2.COLOR_YCrCb2RGB))
+
 
 def encode_color_image(
-        image: np.ndarray,
-        message: bytes,
-        *,
-        coefficients: list[str],
-        alpha: float = 1,
-        block_size: int = 3,
-        level: int = 2,
-        wavelet: str = "haar",
-        color_space: str,
-        use_channels: list[int],
-        ecc_symbols: int,
-        **kwargs,
+    image: np.ndarray,
+    message: bytes,
+    *,
+    coefficients: list[str],
+    alpha: float = 1,
+    block_size: int = 3,
+    level: int = 2,
+    wavelet: str = "haar",
+    color_space: str,
+    use_channels: list[int],
+    ecc_symbols: int,
+    **kwargs,
 ) -> (np.ndarray, np.ndarray, list[bytearray]):
     """Encodes a message into a color image."""
     parameters = {
@@ -44,35 +70,31 @@ def encode_color_image(
 
     ecc_message = msg_utils.encode_ecc(message, **parameters)
 
-    image, _ = blocking.crop_image_to_divisible(image, block_size * 2 ** level)
+    image, _ = blocking.crop_image_to_divisible(image, block_size * 2**level)
 
     message_parts = coder.uniform_message_dispatcher(image, ecc_message, **parameters)
-    channels = list(
-        cv2.split(
-            cv2.cvtColor(image, color_spaces.get(color_space, cv2.COLOR_RGB2YCrCb))
-        )
-    )
+    channels = list(cv2.split(rgb_to_color_space(image, color_space)))
 
     for channel_index in use_channels:
         channel = channels[channel_index]
         encoded_channel = coder.encode(channel, message_parts, **parameters)
         channels[channel_index] = encoded_channel
 
-    stego = cv2.cvtColor(cv2.merge(channels), cv2.COLOR_YCrCb2RGB)
+    stego = color_space_to_rgb(cv2.merge(channels), color_space)
     return image, stego, message_parts
 
 
 def decode_color_image(
-        image: np.ndarray,
-        *,
-        coefficients: list[str],
-        block_size: int = 3,
-        level: int = 2,
-        wavelet: str = "haar",
-        color_space: str,
-        use_channels: list[int],
-        ecc_symbols: int,
-        **kwargs,
+    image: np.ndarray,
+    *,
+    coefficients: list[str],
+    block_size: int = 3,
+    level: int = 2,
+    wavelet: str = "haar",
+    color_space: str,
+    use_channels: list[int],
+    ecc_symbols: int,
+    **kwargs,
 ) -> (bytearray, list[bytes]):
     """Decodes a message from a color image."""
     parameters = {
@@ -83,9 +105,7 @@ def decode_color_image(
         "ecc_symbols": ecc_symbols,
     }
 
-    channels = cv2.split(
-        cv2.cvtColor(image, color_spaces.get(color_space, cv2.COLOR_RGB2YCrCb))
-    )
+    channels = cv2.split(rgb_to_color_space(image, color_space))
 
     message_parts = []
     for channel_index in use_channels:
