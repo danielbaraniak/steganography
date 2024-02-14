8d37975f4b7b4b2230620213e39dc44ba6e84399
Daniel Baraniak <dbaraniak2000@gmail.com>
2024-02-14 02:34:46 +0100

    Vectorize encoding and decoding
    Use center block, not [1,1].
    Do not include center block in mean calculation

--- scripts/embed_batch.py
+++ scripts/embed_batch.py
@@ -13,8 +13,8 @@ SECRET = "Lorem ipsum dolor sit"
 def embed_batch():
     output_dir = Path(config.get_output_dir()) / "test"
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    Path(output_dir / "original_1440").mkdir(parents=True, exist_ok=True)
-    Path(output_dir / "stego_1440").mkdir(parents=True, exist_ok=True)
+    Path(output_dir / "original").mkdir(parents=True, exist_ok=True)
+    Path(output_dir / "stego").mkdir(parents=True, exist_ok=True)
 
     img_dir_path = config.get_images_dir()
     file_names = config.get_images_list()
@@ -27,7 +27,7 @@ def embed_batch():
         print(file_name)
         img_path = img_dir_path + file_name
         img = cv2.imread(str(img_path), cv2.IMREAD_COLOR)
-        img = utils.resize_image_with_aspect_ratio(img, 1440)
+        # img = utils.resize_image_with_aspect_ratio(img, 1440)
         original, stego_image, msg_parts = utils.embed(img, SECRET, encoder_config)
 
         for ext in ["jpg", "png"]:
@@ -38,17 +38,17 @@ def embed_batch():
 
             measurements.append(info)
 
-            output_original_path = output_dir / "original_1440" / info["file_name_new"]
-            output_stego_path = output_dir / "stego_1440" / info["file_name_new"]
+            output_original_path = output_dir / "original" / info["file_name_new"]
+            output_stego_path = output_dir / "stego" / info["file_name_new"]
 
             cv2.imwrite(
-                str(output_original_path), original,  # [int(cv2.IMWRITE_JPEG_QUALITY), 100]
+                str(output_original_path), original, [int(cv2.IMWRITE_JPEG_QUALITY), 100]
             )
             cv2.imwrite(
-                str(output_stego_path), stego_image,  # [int(cv2.IMWRITE_JPEG_QUALITY), 100]
+                str(output_stego_path), stego_image, [int(cv2.IMWRITE_JPEG_QUALITY), 100]
             )
 
-    utils.save_data_to_csv(measurements, str(output_dir / "metadata_1440.csv"))
+    utils.save_data_to_csv(measurements, str(output_dir / "metadata.csv"))
 
 
 if __name__ == "__main__":
