68eadb7dd44182b8ff9b3c2009ac81b435001dec
Daniel Baraniak <dbaraniak2000@gmail.com>
2024-02-18 22:41:19 +0100

    Save to specific folders

--- scripts/decode_batch.py
+++ scripts/decode_batch.py
@@ -6,63 +6,90 @@ import cv2
 from scripts import utils
 from scripts.embed_batch import SECRET
 from stego import config
-from stego.core import metrics
 from stego.core import message as msg_utils
+from skimage import metrics
 
 
-def decode_batch(size):
-    output_dir = Path(config.get_output_dir()) / "test" / f"{size}x{size}"
-    output_dir.mkdir(parents=True, exist_ok=True)
+names_mapping = {
+    "1.jpg": "baloons",
+    "2.jpg": "billiard_balls_b",
+    "3.jpg": "cards_b",
+    "4.jpg": "cushions",
+    "5.jpg": "garden_table",
+    "6.jpg": "pillar",
+    "7.jpg": "scarf",
+    "8.jpg": "sweets",
+    "9.jpg": "tools_b",
+}
 
-    processed_dir_path = output_dir / "processed"
-    stego_dir_path = output_dir / "stego"
+
+def decode_batch(size, file_size):
+    output_dir = Path(config.get_output_dir()) / "test_platforms" / f"{size}x{size}"
+
     encoder_config = config.get_encoder_config()
-
-    file_names = [f.name for f in processed_dir_path.iterdir() if f.is_file()]
 
     results = []
 
-    for file_name in file_names:
-        print(file_name)
+    for platform in ["facebook", "instagram"]:
+        processed_dir_path = output_dir / platform
+        stego_dir_path = output_dir / "stego" / "jpg"
 
-        stego_img_path = stego_dir_path / file_name
-        processed_img_path = processed_dir_path / file_name
+        file_names = [f.name for f in processed_dir_path.iterdir() if f.is_file()]
 
-        stego_img = cv2.imread(str(stego_img_path), cv2.IMREAD_COLOR)
-        processed_img = cv2.imread(str(processed_img_path), cv2.IMREAD_COLOR)
+        for file_name in file_names:
+            print(file_name)
 
-        # processed_img = utils.resize_image_with_aspect_ratio(processed_img, 2016)
+            processed_img_path = processed_dir_path / file_name
+            stego_img_path = stego_dir_path / file_name
 
-        message, ecc_message, message_raw = utils.decode(
-            processed_img, encoder_config
-        )
-        print(message)
+            processed_img = cv2.imread(str(processed_img_path), cv2.IMREAD_COLOR)
+            stego_img = cv2.imread(str(stego_img_path), cv2.IMREAD_COLOR)
 
-        ecc_message_original = msg_utils.encode_ecc(
-            SECRET.encode("ASCII"), **encoder_config
-        )
-        correct_bytes_count = sum(
-            1
-            for msg, msg_decoded in zip(ecc_message_original, ecc_message)
-            if msg == msg_decoded
-        )
-        result = {
-            "file_name": file_name,
-            "message": message,
-            "correct_bytes": correct_bytes_count / len(ecc_message_original),
-        }
+            matched_size_processed = utils.resize_image_with_aspect_ratio(
+                processed_img, file_size
+            )
+            psnr = metrics.peak_signal_noise_ratio(stego_img, matched_size_processed)
 
-        info = metrics.diff_metrics(stego_img, processed_img)
-        result |= info
+            message, ecc_message, message_raw = utils.decode(
+                processed_img, encoder_config
+            )
+            print(message)
 
-        results.append(result)
+            ecc_message_original = msg_utils.encode_ecc(
+                SECRET.encode("ASCII"), **encoder_config
+            )
+            correct_bytes_count = sum(
+                1
+                for msg, msg_decoded in zip(ecc_message_original, ecc_message)
+                if msg == msg_decoded
+            )
+            result = {
+                "processed_dir": processed_dir_path,
+                "image_name": names_mapping[file_name],
+                "is_success": message == SECRET,
+                "correct_bytes": correct_bytes_count / len(ecc_message_original),
+                "platform": platform,
+                "size": processed_img.shape[0],
+                "size_original": size,
+                "psnr": psnr,
+                "resized": False,
+            }
 
-    utils.save_data_to_csv(results, str(output_dir / "decoding_results.csv"))
+            results.append(result)
+
+    return results
 
 
 if __name__ == "__main__":
     start_time = time()
-    decode_batch(2448)
+    results = []
+    for size in [(1440, 1440), (2048, 2040), (2448, 2440)]:
+        results += decode_batch(*size)
+
+    utils.save_data_to_csv(
+        results,
+        Path(config.get_output_dir()) / "test_platforms" / "decoding_results.csv",
+    )
     end_time = time()
     elapsed_time = end_time - start_time
     print(f"Execution time: {elapsed_time:.2f} seconds")
